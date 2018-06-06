69c75218293b5d9215c01ba586030465a2544932
Said Sef <saidsef@gmail.com>
2018-06-06 23:21:08 +0100

    bugfix: build script

--- build.sh
+++ build.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env sh
 
-set -e pipefail
+set -e
 
 BUILD_ID=$1
-
+echo ${BUILD_ID}
 if ! [ -x "$(command -v docker)" ]; then
   echo 'Unable to find docker command, please install Docker (https://www.docker.com/) and retry' >&2
   exit 1
 fi
 
-if ! [ -x "${BUILD_ID}" ]; then
+if [ -z "${BUILD_ID}" ]; then
   echo "Build ID is missing"
   exit 1
 fi
 
 build() {
   echo "Building image"
-  docker build -t saidsef/faas-reverse-geocoding:${BUILD_ID} .
-  docker tag saidsef/faas-reverse-geocoding:${BUILD_ID} saidsef/faas-reverse-geocoding:latest
+  docker build -t saidsef/faas-reverse-geocoding .
+  docker tag saidsef/faas-reverse-geocoding saidsef/faas-reverse-geocoding:${BUILD_ID}
 }
 
 push() {
