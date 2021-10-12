b3c2ca7a944fd2e21e7e614dd24dcb29b9f27b7b
Said Sef <saidsef@gmail.com>
2021-10-13 00:45:45 +0100

    added basic test for request

---
+++ geocode_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestLatitudeLongitude(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	w := httptest.NewRecorder()
+	latitudeLongitude(w, req)
+	res := w.Result()
+	defer res.Body.Close()
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("expected error to be nil got %v", err)
+	}
+	if string(data) == "" {
+		t.Errorf("expected status got %v", string(data))
+	}
+}
