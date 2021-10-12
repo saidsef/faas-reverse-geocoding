3fbe97946c322fb485372bf661f7067f0dbd6a43
Said Sef <saidsef@gmail.com>
2021-10-12 21:38:42 +0100

    replaced stdin with web server
    This is a major change:
    - Added flag to pass cmd args
    - Renamed lng to lon
    - Added prom client instrumentation for /metrics endpoint
    - Added GET and POST handlers
    - Replaced Google Maps with openstreetmap

--- geocode.go
+++ geocode.go
@@ -3,64 +3,80 @@ package main
 import (
 	"crypto/tls"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
-	"os"
-	"strings"
 
-	_ "github.com/joho/godotenv/autoload"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var (
-	key = os.Getenv("KEY")
+	key  string
+	port string
 )
 
 // Coordinates set Lat and Long
 type Coordinates struct {
 	Lat  string `json:"lat"`
-	Long string `json:"lng"`
+	Long string `json:"lon"`
+}
+
+func latitudeLongitude(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var c Coordinates
+
+	switch r.Method {
+	case "GET":
+		fmt.Fprintf(w, `{"status": "healthy"}`)
+	case "POST":
+		err := json.NewDecoder(r.Body).Decode(&c)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		if len(c.Lat) == 0 && len(c.Long) == 0 {
+			fmt.Fprintf(w, "Lat and/or Lon positions error - not set")
+			return
+		}
+
+		tr := &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+
+		client := &http.Client{Transport: tr}
+		url := fmt.Sprintf("https://nominatim.openstreetmap.org/reverse?format=json&zoom=18&addressdetails=1&lat=%s&lon=%s", c.Lat, c.Long)
+		resp, err := client.Get(url)
+		if err != nil {
+			fmt.Fprintf(w, "http get error %s", err)
+		}
+
+		defer resp.Body.Close()
+
+		if resp.StatusCode == http.StatusOK {
+			bodyBytes, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				fmt.Fprintf(w, "body error %s", err)
+			}
+			bodyString := string(bodyBytes)
+			fmt.Fprintf(w, bodyString)
+		}
+	default:
+		fmt.Fprintf(w, `{"status": "method not allowed"}`)
+	}
 }
 
 func main() {
-	if key == "" || len(strings.TrimSpace(key)) == 0 {
-		fmt.Println("key is missing, please set env var `KEY`")
-		return
-	}
+	flag.StringVar(&port, "port", "8080", "Listening PORT")
+	flag.Parse()
 
-	j := Coordinates{}
-	v, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
-		fmt.Printf("input read all error %s", err)
-	}
+	r := http.NewServeMux()
 
-	err = json.Unmarshal(v, &j)
-	if err != nil {
-		fmt.Printf("josn unmarshall error %s", err)
-	}
+	r.HandleFunc("/", latitudeLongitude)
+	r.Handle("/metrics", promhttp.Handler())
 
-	if len(j.Long) == 0 && len(j.Lat) == 0 {
-		fmt.Printf("Lat and/or Lng positions error - not set")
-		return
-	}
-
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-	client := &http.Client{Transport: tr}
-	resp, err := client.Get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + j.Lat + "," + j.Long + "&key=" + key)
-	if err != nil {
-		fmt.Printf("http get error %s", err)
-	}
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode == http.StatusOK {
-		bodyBytes, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Printf("body error %s", err)
-		}
-		bodyString := string(bodyBytes)
-		fmt.Println(bodyString)
-	}
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/saidsef/faas-reverse-geocoding
 
 go 1.16
 
-require github.com/joho/godotenv v1.3.0
+require (
+	github.com/prometheus/client_golang v1.11.0 // direct
+)
