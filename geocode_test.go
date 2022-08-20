3d07ffe8fed4c14fd0a12d63a458a0244b1b48d1
Said Sef <saidsef@gmail.com>
2022-08-20 09:50:34 +0000

    Update test cases
    Signed-off-by: Said Sef <saidsef@gmail.com>

--- geocode_test.go
+++ geocode_test.go
@@ -1,11 +1,27 @@
 package main
 
 import (
-	"io/ioutil"
+	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 )
+
+type TestCoordinates struct {
+	Lat  string
+	Long string
+}
+
+func isCoordinates(t interface{}) bool {
+	switch t.(type) {
+	case TestCoordinates:
+		return true
+	default:
+		return false
+	}
+}
 
 func TestLatitudeLongitude(t *testing.T) {
 	req := httptest.NewRequest(http.MethodGet, "/", nil)
@@ -13,11 +29,20 @@ func TestLatitudeLongitude(t *testing.T) {
 	latitudeLongitude(w, req)
 	res := w.Result()
 	defer res.Body.Close()
-	data, err := ioutil.ReadAll(res.Body)
+	data, err := io.ReadAll(res.Body)
 	if err != nil {
 		t.Errorf("expected error to be nil got %v", err)
 	}
 	if string(data) == "" {
 		t.Errorf("expected status got %v", string(data))
 	}
+}
+
+func TestMain(m *testing.M) {
+
+	t := TestCoordinates{"41.40338", "2.17403"}
+	fmt.Println(isCoordinates(t))
+
+	exitEval := m.Run()
+	os.Exit(exitEval)
 }
