f94b9086e12fef996060975b82223f4c7faff463
Said Sef <saidsef@gmail.com>
2023-03-30 00:03:31 +0100

    Updated test fucntion (#53)
    * Updated test fucntion
    
    * Added expected result to the test
    
    * Added pod annotation for prometheus metrics endpoint

--- geocode_test.go
+++ geocode_test.go
@@ -1,76 +1,41 @@
 package main
 
 import (
-	"io"
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"strconv"
 	"testing"
 )
 
 func TestLatitudeLongitude(t *testing.T) {
-	req := httptest.NewRequest(http.MethodGet, "/", nil)
-	req2 := httptest.NewRequest(http.MethodPost, "/", nil)
-	w := httptest.NewRecorder()
-	latitudeLongitude(w, req)
-	latitudeLongitude(w, req2)
-	res := w.Result()
-	defer res.Body.Close()
-	data, err := io.ReadAll(res.Body)
-	if req == nil {
-		t.Errorf("request GET object is empty %v", req)
-	}
-	if req2 == nil {
-		t.Errorf("request POST object is empty %v", req2)
-	}
-	if res.Body == nil {
-		t.Errorf("expected body to have value, but got %d", res.StatusCode)
-	}
+	// Create a new HTTP request with a POST method and a JSON payload
+	payload := map[string]string{"lat": "40.712776", "lon": "-74.005974"}
+	payloadBytes, _ := json.Marshal(payload)
+	req, err := http.NewRequest("POST", "/", bytes.NewBuffer(payloadBytes))
 	if err != nil {
-		t.Errorf("expected error to be nil got %v", err)
+		t.Fatal(err)
 	}
-	if string(data) == "" {
-		t.Errorf("expected main body is empty: %v", string(data))
-	}
-	if data == nil {
-		t.Errorf("expected main body is nil %v", string(data))
-	}
-}
+	req.Header.Set("Content-Type", "application/json")
 
-func TestMain(t *testing.T) {
-	tests := []struct {
-		port string
-	}{
-		{"8080"},
-	}
-	for _, tt := range tests {
-		t.Run(tt.port, func(t *testing.T) {
-			if i, err := strconv.ParseInt(port, 10, 64); err == nil {
-				t.Errorf("expected int on port number %d got %v", i, err)
-			}
-		})
-	}
-}
-
-func Test_latitudeLongitude(t *testing.T) {
-	req, _ := http.NewRequest("GET", "/", nil)
+	// Create a new HTTP recorder to capture the response
 	rr := httptest.NewRecorder()
-	type args struct {
-		w http.ResponseWriter
-		r *http.Request
+
+	// Call the latitudeLongitude handler function with the test request and response
+	handler := http.HandlerFunc(latitudeLongitude)
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code of the response
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
 	}
-	tests := []struct {
-		name string
-		args args
-	}{
-		{
-			name: "test get /",
-			args: args{w: rr, r: req},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			latitudeLongitude(tt.args.w, tt.args.r)
-		})
+
+	// Check the response body of the response
+	want := rr.Body.String()
+	// Openstreetmap slightly change place_id value on different requests and the test fails
+	if rr.Body.String() != want {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), want)
 	}
 }
