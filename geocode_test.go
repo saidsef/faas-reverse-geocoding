fababf487a3108c19f07be65ec7cbef9ca917d70
Said Sef <saidsef@gmail.com>
2023-04-30 12:34:12 +0100

    Increased test coverage, this will handle invalid body and missing lat long

--- geocode_test.go
+++ geocode_test.go
@@ -64,3 +64,48 @@ func TestPostRequest(t *testing.T) {
 		t.Errorf("handler returned unexpected response: %v", response)
 	}
 }
+
+func TestInvalidRequestBody(t *testing.T) {
+	invalidBody := "invalid"
+
+	req, err := http.NewRequest("POST", "/", bytes.NewBuffer([]byte(invalidBody)))
+	if err != nil {
+		t.Fatalf("could not create request: %v", err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(latitudeLongitude)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusBadRequest)
+	}
+}
+
+func TestMissingLatLong(t *testing.T) {
+	c := Coordinates{}
+	payload, _ := json.Marshal(c)
+
+	req, err := http.NewRequest("POST", "/", bytes.NewBuffer(payload))
+	if err != nil {
+		t.Fatalf("could not create request: %v", err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(latitudeLongitude)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := "Lat and/or Lon positions error - not set"
+	if strings.TrimSpace(rr.Body.String()) != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
